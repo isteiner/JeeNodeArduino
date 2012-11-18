7ced9904f6d1b5ff12ab1d22153ca7b2bbf0ff50
Igor Steiner <isteiner067@gmail.com>
2012-11-18 20:04:30 +0100

    Changed structure payload

--- roomNode_Pressure_HYT131_STI20121118/roomNode_Pressure_HYT131_STI20121118.ino
+++ roomNode_Pressure_HYT131_STI20121118/roomNode_Pressure_HYT131_STI20121118.ino
@@ -56,8 +56,8 @@ struct {
     byte light;       // light sensor: 0..255
     byte moved   :1;  // motion detector: 0..1
     byte humi    :7;  // humidity: 0..100
-    int temp     :10; // temperature: -500..+500 (tenths)
-    int pressure :16; // pressure: hPa    
+    int temp     :16; // temperature: -500..+500 (tenths)
+    int pressure :15; // pressure: hPa    
     byte lobat   :1;  // supply voltage dropped under 3.1V: 0..1
 } payload;
 
