8ee272c1bb5f6016da211dfe595302a812975bbb
Igor Steiner <isteiner067@gmail.com>
2013-04-21 17:55:13 +0200

    Change to group 100 and set TXENAB for RFM12BP

--- RF_Counter/RF_Counter.ino
+++ RF_Counter/RF_Counter.ino
@@ -4,17 +4,24 @@ byte value=0;
 
 void setup () {
   // this is node 15 in net group 212 on the 868 MHz band
-  rf12_initialize(15, RF12_868MHZ, 212);
+  rf12_initialize(15, RF12_868MHZ, 100);
   Serial.begin(57600);
   Serial.println("\n[RF_Counter]");  
+  // set output pin for TXENAB 
+  pinMode(9, OUTPUT);  
 }
 
 void loop () {
   // increment counter
   value++;
-
+  
+  // Switch TXENAB to high
+  digitalWrite(9, HIGH);
+  delay(10);
   // actual packet send: broadcast to all, current counter, 1 byte long
   rf12_sendNow(0, &value, 1);
+  delay(10);
+  digitalWrite(9, LOW);
   
   Serial.println(value);
   
