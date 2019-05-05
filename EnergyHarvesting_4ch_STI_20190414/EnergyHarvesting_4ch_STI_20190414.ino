b37cdd9951454d809c519a09b6af9ccfffaa6b9b
isteiner <isteiner067@gmail.com>
2019-05-05 13:11:06 +0200

    Update EnergyHarvesting_4ch_STI_20190414.ino
    discharge battery, temperature of DS18B20 and payload changed

--- EnergyHarvesting_4ch_STI_20190414/EnergyHarvesting_4ch_STI_20190414.ino
+++ EnergyHarvesting_4ch_STI_20190414/EnergyHarvesting_4ch_STI_20190414.ino
@@ -7,25 +7,38 @@
 // 2019-04-14 <isteiner> EnergyHarvesting_4ch_STI_20190414, monitoring LTC3331 energy harvesting IC
 // 2019-04-21 <isteiner> connect PGVOUT and EHOUT 
 // 2019-04-27 <isteiner> sleep and SERIAL
+// 2019-05-04 <isteiner> discharge battery, temperature of DS18B20 and payload changed 
 
 #include <JeeLib.h>
 #include "Ports.h"
+#include <OneWire.h>
+#include <DallasTemperature.h>
+// Data wire is plugged into  port 7 on the Arduino -> PORT 4 on JeeNode
+#define ONE_WIRE_BUS 7
 
-#define SERIAL  0   // set to 1 to also report readings on the serial port
-
+#define SERIAL  0     // set to 1 to also report readings on the serial port
+#define SWITCH_LOAD 1 // set to 1 to manage discharging of battery
+ 
 PortI2C myI2C (1);
 AnalogPlug adc (myI2C, 0x68);
 
+// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
+OneWire oneWire(ONE_WIRE_BUS);
+
+// Pass our oneWire reference to Dallas Temperature. 
+DallasTemperature sensors(&oneWire);
+
 Port two (2);
 Port three (3);
-Port four (4);
+//Port four (4);
 
 // this must be added since we're using the watchdog for low-power waiting
 ISR(WDT_vect) { Sleepy::watchdogEvent(); }
 
 // Payload data structure:
 struct {
-    int AD_Channel[4];  // mVolts    
+    int AD_Channel[4];  // mVolts   
+    int temperature;    // internal tempearture  
     byte DIO;           // digital inputs     
 } payload;
 
@@ -57,16 +70,7 @@ static void serialFlush () {
     delay(10); // make sure tx buf is empty before going back to sleep
 }
 
-void setup () {
-/*
-#if SERIAL
-    Serial.begin(57600);
-    Serial.print("\n[EnergyHarvesting_4ch_STI_20190414]");  
-    if (adc.isPresent()) 
-      Serial.println("Analog Plug found");    
-    serialFlush();  
-#endif */
-      
+void setup () { 
     cli();                    //cli clears the global interrupt flag in SREG so prevent any form of interrupt occurring. 
     // sets prescaler
     CLKPR = bit(CLKPCE);
@@ -97,17 +101,25 @@ void setup () {
     // initialize the PGVOUT and EHOUT pins as an input:
     two.mode(INPUT);    // PGVOUT on port 2
     three.mode(INPUT);  // EHOUT on port 3
-    four.mode(INPUT);   // spare on port 4
+    //four.mode(INPUT);   // spare on port 4 
+    
+    // Start up the OneWire library
+    sensors.begin();
+    
+    #if SWITCH_LOAD   //discarge battery if this option is selected
+        pinMode(9, OUTPUT);   // build-in LED on PB1 = Arduino pin 9
+        digitalWrite(9, HIGH);  // switch LED off (invert/pullup)                     
+    #endif     
 }
 
 void loop () {
     for (byte i=1; i <= 4; ++i) {
       long val = AP2read(adc, i);
       // calibrating values for CH1..CH4
-      if (i == 1) val = val/13; // 0x1ffff/10020 = 13  CH1 = +- 10020 mV
-      if (i == 2) val = val/24; // 0x1ffff/5660  = 23  CH2 = +- 5660 mV
-      if (i == 3) val = val/24; // 0x1ffff/5660  = 23  CH3 = +- 5660 mV    
-      if (i == 4) val = val/24; // 0x1ffff/5660  = 23  CH4 = +- 5660 mV      
+      if (i == 1) val = val/13; // 0x1ffff/10020 = 13  CH1 = +- 10020 mV  AC1
+      if (i == 2) val = val/24; // 0x1ffff/5660  = 23  CH2 = +- 5660 mV   VBAT
+      if (i == 3) val = val/24; // 0x1ffff/5660  = 23  CH3 = +- 5660 mV   VOUT    
+      if (i == 4) val = val/24; // 0x1ffff/5660  = 23  CH4 = +- 5660 mV   ICHARGE   
       payload.AD_Channel[i-1] = (int)val; 
       #if SERIAL
         Serial.print(val);  
@@ -115,22 +127,38 @@ void loop () {
       #endif      
     }
 
+    sensors.requestTemperatures(); // Send the command to get temperatures
+    // We use the function ByIndex, and as an example get the temperature from the first sensor only.
+    payload.temperature = (int)(sensors.getTempCByIndex(0)*10); 
+    #if SERIAL
+        Serial.print(payload.temperature);  
+        Serial.print(' ');
+    #endif   
+    
+    #if SWITCH_LOAD   //discarge battery if Vbat is high enough
+      if (payload.AD_Channel[1] > 4000) 
+      {
+        payload.DIO |= 0x04;
+        digitalWrite(9, LOW);  // switch LED on if Vbat > 4000 mV (invert/pullup)      
+      }
+      if (payload.AD_Channel[1] < 3700) 
+      {
+        payload.DIO &= 0xFB;
+        digitalWrite(9, HIGH);  // switch LED off if Vbat < 3700 mV (invert/pullup)              
+      }       
+    #endif 
+        
     uint8_t PGVOUT = two.digiRead();
     uint8_t EHOUT = three.digiRead();
-    #if SERIAL    
-      Serial.print(PGVOUT); 
-      Serial.print(' '); 
-      Serial.print(EHOUT);  
-      Serial.print(' '); 
-    #endif  
-    
+
     //set - reset bit b0 = PGVOUT and b1 = EHOUT
     if (PGVOUT) payload.DIO |= 0x01;
     else payload.DIO &= 0xFE; 
     if (EHOUT) payload.DIO |= 0x02;
     else payload.DIO &= 0xFD; 
+
     #if SERIAL
-      Serial.print(payload.DIO, BIN); 
+      Serial.print(payload.DIO, BIN); // b0=PGVOUT, b1=EHOUT, b2=LED discharge 
       Serial.println();
       serialFlush();
     #endif  
